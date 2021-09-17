4de975273146a1a31984aae1be9d6a4322a4e34e
DaanKorver <daank2000@hotmail.nl>
2021-09-17 12:05:48 +0200

    kaarten omgedraaid

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,16 @@ const tl = gsap.timeline({
   onComplete: changeContent
 })
 
-tl.to(".card-1", {duration: .7, y: "-110%"}, "cards")
+tl.to(".card-1", {duration: .7, x: "-110%"}, "cards")
   .set(".card-1", {css:{zIndex: 1}})
-  .to(".card-2", {duration: .7, y: "0%"}, "cards")
-  .to(".card-3", {duration: .7, y: "3%"}, "cards")
-  .to(".card-1", {duration: 1, y: "6%"})
+  .to(".card-2", {duration: .7, x: "0%"}, "cards")
+  .to(".card-3", {duration: .7, x: "3%"}, "cards")
+  .to(".card-1", {duration: 1, x: "6%"})
 
 
 tl.play()
 
-let arr = ["Hello", "Hello2", "Hello3"]
+let arr = ["Daan", "Beau", "Yaser", "Jean"]
 
 const card1 = document.querySelector(".card-1")
 const card2 = document.querySelector(".card-2")
