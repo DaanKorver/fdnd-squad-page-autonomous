72ec82a2af0075997c54a3f4ef4982e7de5b90e7
beaupd <beau@patrickdekker.nl>
2021-09-20 14:11:16 +0200

    menu werkt functionaliteit

--- scripts/script.js
+++ scripts/script.js
@@ -32,6 +32,27 @@ function changeContent() {
 // changeContent()
 
 
-function setActive(e) {
-  
+
+fetch("../assets/persons.json").then(res=>{
+  return res.json()
+}).then(data=>{
+  console.log(data)
+})
+
+function setActive(elem, i) {
+  deActive();
+  document.querySelector("nav").classList.add("active");
+  elem.parentNode.classList.add("active-item");
+  let content = document.querySelectorAll("nav .content-container li")[i].classList.add("active");
+  console.log("activated", elem);
+}
+
+function deActive() {
+  document.querySelector("nav").classList.remove("active");
+  document.querySelectorAll("nav .content-container li").forEach((item)=>{
+    item.classList.remove("active");
+  });
+  document.querySelectorAll("nav li").forEach((item) => {
+    item.classList.remove("active-item");
+  });
 }
